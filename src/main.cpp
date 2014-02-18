7e213a803d2ea943e930807b566324675c53591c
maddinat0r <maddin4t0r@gmail.com>
2014-02-18 17:50:03 +0100

    push revision number

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData)
 	
 	CLog::Get()->Initialize("mysql_log.txt"); 
 
-	logprintf(" >> plugin.mysql: R37 successfully loaded.");
+	logprintf(" >> plugin.mysql: R38 successfully loaded.");
 	return true;
 }
 
