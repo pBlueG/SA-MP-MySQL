a9575199258c890812d457e6faabcbab8961d216
maddinat0r <maddin4t0r@gmail.com>
2014-10-30 19:05:10 +0100

    add unit test for connection handle ID distribution

--- unit-test/src/main.cpp
+++ unit-test/src/main.cpp
@@ -7,6 +7,8 @@
 #include "../../src/CCallback.h"
 #include "../../src/CResult.h"
 
+#include <vector>
+
 
 struct GlobalSetup
 {
@@ -85,6 +87,90 @@ BOOST_AUTO_TEST_SUITE(Handle)
 		handle = CHandleManager::Get()->Create(host, user, password, database, port, 33, handle_error);
 		BOOST_CHECK(handle_error == CHandle::Error::INVALID_POOL_SIZE);//, L"wrong handle-error");
 		BOOST_CHECK(handle == nullptr);//, L"handle pointer not null");
+	}
+	BOOST_FIXTURE_TEST_CASE(IdEnumeration, HandleData)
+	{
+		//check if id's are correctly distributed
+
+		//create first handle -> id should be 1
+		CHandle
+			*first_handle = handle = CHandleManager::Get()->Create(host, user, password, database, port, pool_size, handle_error),
+			*second_handle = nullptr,
+			*third_handle = nullptr;
+
+		BOOST_CHECK(handle_error == CHandle::Error::NONE);
+		BOOST_CHECK_EQUAL(handle->GetId(), 1);
+
+		//create 9 other handles, id's should be 2-10
+		std::vector<decltype(handle)> handles;
+		for (size_t i = 2; i <= 10; ++i)
+		{
+			handle = CHandleManager::Get()->Create(host, user, password, database, port, pool_size, handle_error);
+			BOOST_CHECK(handle_error == CHandle::Error::NONE);
+			BOOST_CHECK_EQUAL(handle->GetId(), i);
+
+			if (i != 2)
+				handles.push_back(handle);
+			else
+				second_handle = handle;
+		}
+
+		//delete 3-10, 2 is 'second_handle'
+		for (auto &h : handles)
+		{
+			BOOST_CHECK(CHandleManager::Get()->Destroy(h));
+		}
+		handles.clear();
+
+		//create 3-10
+		for (size_t i = 3; i <= 10; ++i)
+		{
+			handle = CHandleManager::Get()->Create(host, user, password, database, port, pool_size, handle_error);
+			BOOST_CHECK(handle_error == CHandle::Error::NONE);
+			BOOST_CHECK_EQUAL(handle->GetId(), i);
+
+			if (i != 3)
+				handles.push_back(handle);
+			else
+				third_handle = handle;
+		}
+
+		//delete 1 ('first_handle')
+		BOOST_CHECK(CHandleManager::Get()->Destroy(first_handle));
+
+		//re-create 1
+		first_handle = handle = CHandleManager::Get()->Create(host, user, password, database, port, pool_size, handle_error);
+		BOOST_CHECK(handle_error == CHandle::Error::NONE);
+		BOOST_CHECK_EQUAL(handle->GetId(), 1);
+
+		//delete 4-10 (3 is 'third_handle')
+		for (auto &h : handles)
+		{
+			BOOST_CHECK(CHandleManager::Get()->Destroy(h));
+		}
+		handles.clear();
+
+		//check if our three handles are present and valid
+		BOOST_CHECK(first_handle != nullptr);
+		BOOST_CHECK_EQUAL(first_handle->GetId(), 1);
+		BOOST_CHECK(second_handle != nullptr);
+		BOOST_CHECK_EQUAL(second_handle->GetId(), 2);
+		BOOST_CHECK(third_handle != nullptr);
+		BOOST_CHECK_EQUAL(third_handle->GetId(), 3);
+
+		//delete 2
+		BOOST_CHECK(CHandleManager::Get()->Destroy(second_handle));
+
+		//re-create 2
+		second_handle = handle = CHandleManager::Get()->Create(host, user, password, database, port, pool_size, handle_error);
+		BOOST_CHECK(handle_error == CHandle::Error::NONE);
+		BOOST_CHECK_EQUAL(handle->GetId(), 2);
+
+		//clean-up
+		BOOST_CHECK(CHandleManager::Get()->Destroy(first_handle));
+		BOOST_CHECK(CHandleManager::Get()->Destroy(second_handle));
+		BOOST_CHECK(CHandleManager::Get()->Destroy(third_handle));
+
 	}
 //}
 BOOST_AUTO_TEST_SUITE_END()
