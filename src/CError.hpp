f609cf5914a0f1b68efad2de792072ce389e1e76
Alex Martin <maddin4t0r@gmail.com>
2015-08-30 16:20:40 +0200

    add "set" method with formatting options

--- src/CError.hpp
+++ src/CError.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <format.h>
 
 using std::string;
 
@@ -37,6 +38,12 @@ public:
 	{
 		m_Type = type;
 		m_Message.assign(std::move(msg));
+	}
+	template<typename... Args>
+	void set(ErrorType type, string &&format, Args &&...args)
+	{
+		m_Type = type;
+		m_Message.assign(fmt::format(format, std::forward<Args>(args)...));
 	}
 
 private:
