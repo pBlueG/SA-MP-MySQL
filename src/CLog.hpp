0fcb48807a0d4a84b3517386ac90ad1d09fcf86c
Alex Martin <maddin4t0r@gmail.com>
2015-08-30 18:08:41 +0200

    improve CLog class
    - added some extra methods for logging
    - made use of the cppformat library
    - made pointer atomic (better safe than sorry)

--- src/CLog.hpp
+++ src/CLog.hpp
@@ -4,8 +4,12 @@
 #include "CSingleton.hpp"
 #include "CError.hpp"
 
+#include <format.h>
+#include <atomic>
+
 using samplog::PluginLogger_t;
 using samplog::LOGLEVEL;
+
 
 class CLog : public CSingleton<CLog>
 {
@@ -16,32 +20,52 @@ private:
 
 public:
 	template<typename... Args>
-	void LogFormat(const LOGLEVEL &level, const std::string &fmt, Args ...args)
+	void Log(const LOGLEVEL &level, const std::string &fmt, Args &&...args)
 	{
-		char dest[2048];
-		sprintf(dest, fmt.c_str(), args...);
-		m_Logger->Log(level, dest);
+		m_Logger.load(std::memory_order_acquire)
+			->Log(level, fmt::format(fmt, std::forward<Args>(args)...));
 	}
 	template<typename... Args>
-	void LogFormat(AMX * const amx, const LOGLEVEL &level, const std::string &fmt, Args ...args)
+	void Log(AMX * const amx, const LOGLEVEL &level, const std::string &fmt, Args &&...args)
 	{
-		char dest[2048];
-		sprintf(dest, fmt.c_str(), args...);
-		m_Logger->Log(amx, level, dest);
+		m_Logger.load(std::memory_order_acquire)
+			->Log(amx, level, fmt::format(fmt, std::forward<Args>(args)...));
 	}
 	
 	inline bool LogNativeCall(AMX * const amx, const std::string &name, const std::string &params_format)
 	{
-		return m_Logger->LogNativeCall(amx, name, params_format);
+		return m_Logger.load(std::memory_order_acquire)
+			->LogNativeCall(amx, name, params_format);
 	}
 
 	template<typename T>
 	inline void LogError(AMX * const amx, const std::string &func, const CError<T> &error)
 	{
-		LogFormat(amx, LOGLEVEL::ERROR, "%s: %s error: %s", 
+		Log(amx, LOGLEVEL::ERROR, "{}: {} error: {}",
 			func.c_str(), error.module().c_str(), error.msg().c_str());
+	}
+	inline void LogError(AMX * const amx, const std::string &func, const std::string &msg)
+	{
+		Log(amx, LOGLEVEL::ERROR, "{}: {}", func.c_str(), msg.c_str());
+	}
+	template<typename... Args>
+	inline void LogError(AMX * const amx, const std::string &func, 
+		const std::string &format, Args &&...args)
+	{
+		LogError(amx, func, fmt::format(format, std::forward<Args>(args)...));
+	}
+
+	inline void LogWarning(AMX * const amx, const std::string &func, const std::string &msg)
+	{
+		Log(amx, LOGLEVEL::WARNING, "{}: {}", func.c_str(), msg.c_str());
+	}
+	template<typename... Args>
+	inline void LogError(AMX * const amx, const std::string &func,
+		const std::string &format, Args &&...args)
+	{
+		LogError(amx, func, fmt::format(format, std::forward<Args>(args)...));
 	}
 
 private:
-	PluginLogger_t m_Logger;
+	std::atomic<PluginLogger_t> m_Logger;
 };
